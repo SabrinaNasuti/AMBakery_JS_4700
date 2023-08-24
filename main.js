41db7d089b2b4933209d7315da646aa5fd403b9b
Sabrina Nasuti <snasuti90@gmail.com>
2023-08-23 21:19:26 -0300

    Funciones de filstrado

--- main.js
+++ main.js
@@ -3,114 +3,119 @@ const catalogoCompleto = [
   {
     idCatalogo: 1,
     nombreCatalogo: "Torta Ensueño",
-    precioCatalogo: 2500,
+    precioCatalogo: "$2500",
     imagenCatalogo:
       "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417150/desarrollo_web/producto-1_os8f08.jpg",
     seccionCatalogo: {
       idSeccion: 1,
-      nombrSeccion: "secTortas",
+      nombrSeccion: "Tortas",
     },
   },
 
   {
     idCatalogo: 2,
     nombreCatalogo: "Torta White Album",
-    precioCatalogo: 2500,
+    precioCatalogo: "$2500",
     imagenCatalogo:
       "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417081/desarrollo_web/producto-3_ohyann.jpg",
     seccionCatalogo: {
       idSeccion: 2,
-      nombrSeccion: "secTortas",
+      nombrSeccion: "Tortas",
     },
   },
   {
     idCatalogo: 3,
     nombreCatalogo: "Torta Rainbow Party",
-    precioCatalogo: 2500,
+    precioCatalogo: "$2500",
     imagenCatalogo:
       "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417075/desarrollo_web/producto-4_lylxw4.webp",
     seccionCatalogo: {
       idSeccion: 3,
-      nombrSeccion: "secTortas",
+      nombrSeccion: "Tortas",
     },
   },
 
   {
     idCatalogo: 4,
     nombreCatalogo: "Torta Green Lovers",
-    precioCatalogo: 2500,
+    precioCatalogo: "$2500",
     imagenCatalogo:
       "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417080/desarrollo_web/producto-5_serrrj.jpg",
     seccionCatalogo: {
       idSeccion: 4,
-      nombrSeccion: "secTortas",
+      nombrSeccion: "Tortas",
     },
   },
-  
+
   {
     idCatalogo: 5,
     nombreCatalogo: "Torta Sakura",
-    precioCatalogo: 2500,
+    precioCatalogo: "$2500",
     imagenCatalogo:
       "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417081/desarrollo_web/producto-6_cnlt0v.jpg",
     seccionCatalogo: {
       idSeccion: 5,
-      nombrSeccion: "secTortas",
+      nombrSeccion: "Tortas",
     },
   },
 
   {
     idCatalogo: 5,
     nombreCatalogo: "Violet Evergarden",
-    precioCatalogo: 2500,
+    precioCatalogo: "$2500",
     imagenCatalogo:
       "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417150/desarrollo_web/header-creaciones.jpg",
     seccionCatalogo: {
       idSeccion: 5,
-      nombrSeccion: "secTortas",
+      nombrSeccion: "Tortas",
     },
-  }, 
+  },
 
   {
     idCatalogo: 7,
     nombreCatalogo: "Macarons Stargazers",
-    precioCatalogo: 2500,
-    imagenCatalogo: "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417146/desarrollo_web/producto-7_enwyxn.jpg"
-      ,
+    precioCatalogo: "$2500",
+    imagenCatalogo:
+      "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417146/desarrollo_web/producto-7_enwyxn.jpg",
     seccionCatalogo: {
       idSeccion: 1,
-      nombrSeccion: "secMacarons",
+      nombrSeccion: "Macarons",
     },
   },
   {
     idCatalogo: 8,
     nombreCatalogo: "Macarons Barbie",
-    precioCatalogo: 2500,
+    precioCatalogo: "$2500",
     imagenCatalogo:
       "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417081/desarrollo_web/producto-8_cccfqx.jpg",
     seccionCatalogo: {
       idSeccion: 2,
-      nombrSeccion: "secMacarons",
+      nombrSeccion: "Macarons",
     },
   },
   {
     idCatalogo: 9,
     nombreCatalogo: "Alfajores Cumbre Argentina",
-    precioCatalogo: 1750,
+    precioCatalogo: "$2500",
     imagenCatalogo:
       "https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417081/desarrollo_web/producto-2_bo01mv.jpg",
     seccionCatalogo: {
       idSeccion: 1,
-      nombrSeccion: "secAlfajores",
+      nombrSeccion: "Alfajores",
     },
   },
 ];
 
 const galeriaProductos = document.getElementById("galeria-productos");
 
-/* FUNCION PARA CREAR LOS DIV */
-function mostrarCatalogo() {
-  catalogoCompleto.forEach((catalogo) => {
+const botonesMenu = document.querySelectorAll(".boton");
+
+/* FUNCIONES */
+
+function mostrarCatalogo(productosSeleccionados) {
+  galeriaProductos.innerHTML = "";
+
+  productosSeleccionados.forEach((catalogo) => {
     const div = document.createElement("div");
     div.classList.add("productos");
     div.innerHTML = `
@@ -125,10 +130,25 @@ function mostrarCatalogo() {
     `;
 
     /* Subo los cambios */
-    galeriaProductos.append(div); 
+    galeriaProductos.append(div);
   });
-};
+}
 
-mostrarCatalogo(); 
+mostrarCatalogo(catalogoCompleto);
 
-/*"https://res.cloudinary.com/dfbwcrulp/image/upload/v1676417081/desarrollo_web/producto-6_cnlt0v.jpg"*/
+botonesMenu.forEach((boton) => {
+  boton.addEventListener("click", (e) => {
+    if (
+      e.currentTarget.id === boton.id &&
+      e.currentTarget.id != "creacionesMenu"
+    ) {
+      const catalogoFiltrado = catalogoCompleto.filter(
+        (filtro) => filtro.seccionCatalogo.nombrSeccion === e.currentTarget.id
+      );
+      console.log(catalogoFiltrado);
+      mostrarCatalogo(catalogoFiltrado);
+    } else {
+      mostrarCatalogo(catalogoCompleto);
+    };
+  });
+});
